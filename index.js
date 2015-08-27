bd0b1ccfcb0c4bf72cd1b01c86f0781239575a69
Matt Elia <matt.elia@gramercytech.com>
2015-08-27 19:51:45 -0400

    addd mongoDB w / users and msgs collections

--- index.js
+++ index.js
@@ -2,6 +2,52 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var mongo = require('mongodb');
+
+
+var MongoClient = mongo.MongoClient;
+
+// Connection URL. This is where your mongodb server is running.
+var url = 'mongodb://localhost:27017/pickupDB';
+
+var collect_msgs;
+var collect_users;
+
+// Use connect method to connect to the Server
+MongoClient.connect(url, function (err, db) {
+  if (err) {
+    console.log('Unable to connect to the mongoDB server. Error:', err);
+  } else {
+    //HURRAY!! We are connected. :)
+    console.log('Connection established to', url);
+	
+	var collect_users = db.collection('users');
+	var user1 = {name: 'matt', roles: ['admin', 'user']};
+
+		collect_users.upsert(user1, function (err, result) {
+			if(err) {
+	 			console.log(err);
+	        } else {
+		        console.log('Inserted %d documents into the "users" collection. The documents inserted with "_id" are:', result.length, result);
+		    }
+		});
+
+	var collect_msgs = db.collection('messages');
+	io.on('connection', function(socket) {
+		socket.on('chat message', function(msg){
+			console.log('message: ' + msg);
+			var message = {message: msg, timestamp: new Date()};
+			collect_msgs.insert(message, function (err, result) {
+				if(err){
+					console.log(err);
+				} else {
+					console.log('Inserted %d msgs into the "messages" collection. The documents inserted with "_id" are:', result.length, result);
+				}
+			});
+		});
+	});
+  	}
+});
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
@@ -14,6 +60,7 @@ io.on('connection', function(socket){
 	});
 	socket.on('chat message', function(msg){
 		console.log('message: ' + msg);
+		console.log(msg.length);
 	});
 });
 
